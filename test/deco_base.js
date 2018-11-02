2ab47149293541a1a8e0e64522b14ad3417bda30
Pavel Sipaylo <pavel.sipaylo@gmail.com>
2018-11-02 19:21:14 +0300

    Fixed base contract tests to deploy and use it directly.

--- test/deco_base.js
+++ test/deco_base.js
@@ -1,35 +1,21 @@
 let DecoProjects = artifacts.require("./DecoProjects.sol")
 let DecoMilestones = artifacts.require("./DecoMilestones.sol")
-let DecoRelay = artifacts.require("./DecoRelay.sol");
+let DecoRelay = artifacts.require("./DecoRelay.sol")
+let DecoBaseProjectsMarketplace = artifacts.require("./DecoBaseProjectsMarketplace.sol")
 
 contract("DecoBaseProjectsMarketplace", async (accounts) => {
+  let base = undefined
+
+  beforeEach(async () => {
+    base = await DecoBaseProjectsMarketplace.new({from: accounts[0], gasPrice: 1})
+  })
 
   it("should revert any incoming ETH.", async () => {
-    let decoProjects = await DecoProjects.deployed()
-    let decoMilestones = await DecoMilestones.deployed()
-    let txn = await decoProjects.sendTransaction(
-      {
-        from: accounts[0],
-        value: web3.toWei(1)
-      }
+    let txn = await base.sendTransaction(
+      { from: accounts[0], value: web3.toWei(1) }
     ).catch(async (err) => {
       assert.isOk(err, "Exception should be thrown for any incoming ETH transaction.")
-      let balance = await web3.eth.getBalance(decoProjects.address)
-      expect(balance.toNumber()).to.be.equal(0)
-    }).then((txn) => {
-      if(txn) {
-        assert.fail(txn, "Should have failed above.")
-      }
-    })
-
-    await decoMilestones.sendTransaction(
-      {
-        from: accounts[0],
-        value: web3.toWei(1)
-      }
-    ).catch(async (err) => {
-      assert.isOk(err, "Exception should be thrown for any incoming ETH transaction.")
-      let balance = await web3.eth.getBalance(decoMilestones.address)
+      let balance = await web3.eth.getBalance(base.address)
       expect(balance.toNumber()).to.be.equal(0)
     }).then((txn) => {
       if(txn) {
@@ -39,33 +25,30 @@ contract("DecoBaseProjectsMarketplace", async (accounts) => {
   })
 
   it("should let setting relay contract address by the contract owner.", async () => {
-    let decoProjects = await DecoProjects.deployed()
-    let decoMilestones = await DecoMilestones.deployed()
     let decoRelay = await DecoRelay.deployed()
-    await decoProjects.setRelayContractAddress(
-      decoRelay.address,
-      { from: accounts[0], gasPrice: 1 }
-    )
-    await decoMilestones.setRelayContractAddress(
+    await base.setRelayContractAddress(
       decoRelay.address,
       { from: accounts[0], gasPrice: 1 }
     )
 
-    let newAddress = await decoProjects.relayContractAddress.call()
+    let newAddress = await base.relayContractAddress.call()
     expect(newAddress).to.be.equal(decoRelay.address)
   })
 
   it("should fail setting 0x0 milestones contract address.", async () => {
-    let decoProjects = await DecoProjects.deployed()
     let decoRelay = await DecoRelay.deployed()
     let address = decoRelay.address
+    await base.setRelayContractAddress(
+      address,
+      { from: accounts[0], gasPrice: 1 }
+    )
 
-    await decoProjects.setRelayContractAddress(
+    await base.setRelayContractAddress(
       "0x0",
       { from: accounts[0], gasPrice: 1 }
     ).catch(async (err) => {
       assert.isOk(err, "Exception should be thrown for that transaction.")
-      let newAddress = await decoProjects.relayContractAddress.call()
+      let newAddress = await base.relayContractAddress.call()
       expect(address).to.be.equal(newAddress)
     }).then((txn) => {
       if(txn) {
@@ -75,11 +58,9 @@ contract("DecoBaseProjectsMarketplace", async (accounts) => {
   })
 
   it("should fail setting the relay contract address by not the owner.", async () => {
-    let decoMilestones = await DecoMilestones.deployed()
     let decoRelay = await DecoRelay.deployed()
-    let address = decoRelay.address
 
-    await decoMilestones.setRelayContractAddress(
+    await base.setRelayContractAddress(
       decoRelay.address,
       { from: accounts[8], gasPrice: 1 }
     ).catch(async (err) => {
